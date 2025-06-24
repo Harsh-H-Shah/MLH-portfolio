755080d394ef90078f6401f2000592399959039b
Harsh <harshmkvviv@gmail.com>
2025-06-24 23:51:31 +0530

    Add work experience and education sections (#3, #4)

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Harsh Shah", url=os.getenv("URL"))
+    work_experience = [
+        {"role": "Software Developer Intern", "company": "Aumsat Technologies", "duration": "Jun 2023 – Oct 2023"},
+        {"role": "Frontend Developer Intern", "company": "Skinzy Software", "duration": "Oct 2021 – Dec 2021"}
+    ]
+
+    education = [
+        {"school": "Stony Brook University", "degree": "MS in Computer Science", "duration": "2024 – 2026"},
+        {"school": "University of Mumbai - TSEC", "degree": "BE in Computer Engineering", "duration": "2020 – 2024"}
+    ]
+    return render_template('index.html', title="Harsh Shah", url=os.getenv("URL"), work_experience=work_experience, education=education)
